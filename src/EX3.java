af93cf1f017f80853b5319e5159ca36309b4c0f7
sovanda_m <vanda.can.read@gmail.com>
2024-10-20 18:08:13 +0700

    finished EX3

--- src/EX3.java
+++ src/EX3.java
@@ -6,15 +6,24 @@ public class EX3 {
         System.out.print("How many threads do you want? ");
         int numberOfThreads = Integer.parseInt(scanner.nextLine());
 
+        Thread[] threads = new Thread[numberOfThreads];
+
         for (int i = 0; i < numberOfThreads; i++) {
             int index = i;
             Runnable runnable = () -> {
-                System.out.println("Index " + index);
+                System.out.println(index);
             };
-            Thread thread = new Thread(runnable);
-            thread.start();
+            threads[i] = new Thread(runnable);
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.start();
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         scanner.close();
     }
-
 }
